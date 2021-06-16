c9788f8c7333e0424cf38582549be2bf00c0243e
JaewanJeong <jwmsg0525@gmail.com>
2021-06-16 12:13:37 +0900

    fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,12 +7,24 @@
     </v-row>
     <v-row>
       <v-col cols="12" md="6">
-        <v-btn block color="info" class="py-12" style="font-size: 3rem" @click="goFind">
+        <v-btn
+          block
+          color="info"
+          class="py-12"
+          style="font-size: 3rem"
+          @click="goFind"
+        >
           보물찾기
         </v-btn>
       </v-col>
       <v-col cols="12" md="6">
-        <v-btn block color="success" class="py-12" style="font-size: 3rem" @click="goHide">
+        <v-btn
+          block
+          color="success"
+          class="py-12"
+          style="font-size: 3rem"
+          @click="goHide"
+        >
           보물숨기기
         </v-btn>
       </v-col>
@@ -21,6 +33,7 @@
 </template>
 
 <script>
+import { createTreasures } from "@/api/treasures";
 export default {
   name: "Home",
 
@@ -28,8 +41,21 @@ export default {
 
   methods: {
     goHide() {
+      if (!confirm("이곳에 숨기겠습니까?")) {
+        return;
+      }
       if (this.$store.state.is_logined) {
-        this.$router.push("/hide");
+        navigator.geolocation.getCurrentPosition(async function (position) {
+          const data = await createTreasures(
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          if (data.data.success) alert("숨겼습니다!");
+          else{
+            if(data.data.error) alert(data.data.error)
+            if(data.data.message) alert(data.data.message)
+          }
+        });
       } else {
         alert("로그인 후 이용 가능합니다.");
         this.$router.push("/login");
